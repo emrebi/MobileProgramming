890b5e883eb96e685824a31b7651974a0e9a7ded
Emre <116496887+emrebi@users.noreply.github.com>
2025-06-20 17:51:40 +0300

    Add files via upload

---
+++ PomodoroTimer/src/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.timerapp;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView timerText;
+    private Button startButton;
+    private CountDownTimer countDownTimer;
+    private boolean isRunning = false;
+    private long timeLeftInMillis = 1500000; // 25 dakika
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timerText = findViewById(R.id.timerText);
+        startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isRunning) {
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+    }
+
+    private void startTimer() {
+        countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMillis = millisUntilFinished;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+                isRunning = false;
+                startButton.setText("Start");
+            }
+        }.start();
+
+        isRunning = true;
+        startButton.setText("Pause");
+    }
+
+    private void pauseTimer() {
+        countDownTimer.cancel();
+        isRunning = false;
+        startButton.setText("Start");
+    }
+
+    private void updateTimer() {
+        int minutes = (int) (timeLeftInMillis / 1000) / 60;
+        int seconds = (int) (timeLeftInMillis / 1000) % 60;
+        String timeFormatted = String.format("%02d:%02d", minutes, seconds);
+        timerText.setText(timeFormatted);
+    }
+}
